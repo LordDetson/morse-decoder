2fb7133ed7c277c0ded32d272f91e9103019afae
Lord_Detson <babanin.dima@gmail.com>
2019-10-06 18:18:24 +0300

    feat: add decode morse

--- src/index.js
+++ src/index.js
@@ -37,8 +37,37 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const code_table = {
+    ".":"10",
+    "-":"11"
+};
+
+let binary_morse_table = {"**********": " "};
+
+convertTable();
 function decode(expr) {
-    // write your solution here
+    let result = "";
+    for (let i = 0; i < expr.length; i += 10) {
+        let code = expr.substr(i, 10);
+        result += binary_morse_table[code];
+    }
+
+    return result;
+
+}
+
+function convertTable() {
+    let cKey;
+    for (let key in MORSE_TABLE) {
+        cKey = "";
+        for (let char of key) {
+            cKey += code_table[char];
+        }
+        while (cKey.length < 10) {
+            cKey = "0" + cKey;
+        }
+        binary_morse_table[cKey] = MORSE_TABLE[key];
+    }
 }
 
 module.exports = {
